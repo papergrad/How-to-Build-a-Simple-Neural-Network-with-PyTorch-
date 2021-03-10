641c218ac8481b037939fb854edb135894b0f6a4
PaperGrad <80392225+papergrad@users.noreply.github.com>
2021-03-10 20:24:06 +0530

    Create pytorch.py

---
+++ pytorch.py
@@ -0,0 +1,26 @@
+from torch import nn
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        # Inputs to hidden layer linear transformation
+        self.hidden = nn.Linear(10, 128)
+        # Output layer, 2 units - one for each digit
+        self.output = nn.Linear(128, 2)
+        
+        # Define sigmoid activation and softmax output 
+        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=1)
+        
+    def forward(self, x):
+        # Pass the input tensor through each of our operations
+        x = self.hidden(x)
+        x = self.sigmoid(x)
+        x = self.output(x)
+        x = self.softmax(x)
+        
+        return x
+      
+# Now let's create the network
+model = Network()
+model
